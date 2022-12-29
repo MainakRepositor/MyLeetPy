5582a2e08923622ffb2e3fc6c62b8fae8efd8f37
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2022-12-29 17:37:40 +0530

    Create 50.py

---
+++ 50.py
@@ -0,0 +1,3 @@
+class Solution:
+    def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+        return sum(start <= queryTime <= end for start, end in zip(startTime, endTime))
