449346de37b4f43834896ca7961eb2571939c734
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-01-01 11:05:44 +0530

    Create 94.py

---
+++ 94.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        #
+        set1 = {'q','w','e','r','t','y','u','i','o','p'}
+        set2 = {'a','s','d','f','g','h','j','k','l'}
+        set3 = {'z','x','c','v','b','n','m'}
+        
+        res = []
+        for i in words:
+            wordset = set(i.lower())
+            if (wordset&set1 == wordset) or (wordset&set2 == wordset) or (wordset&set3 == wordset):
+                res.append(i)
+        return res
