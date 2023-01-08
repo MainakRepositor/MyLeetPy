6e46b50a627fa298c073883b75c3603b8dd75d3c
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-01-08 07:49:24 +0530

    Create 194.py

---
+++ 194.py
@@ -0,0 +1,4 @@
+    def isAlienSorted(self, words, order):
+        m = {c: i for i, c in enumerate(order)}
+        words = [[m[c] for c in w] for w in words]
+        return all(w1 <= w2 for w1, w2 in zip(words, words[1:]))
