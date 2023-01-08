e05b52c6cfd42f6b804cf94f33b7267c66bb0d15
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-01-08 07:03:36 +0530

    Create 142.py

---
+++ 142.py
@@ -0,0 +1,10 @@
+# Time complexity: O(n)
+# Space complexity: O(n)
+class Solution(object):
+    def containsDuplicate(self, nums):
+        hset = set()
+        for idx in nums:
+            if idx in hset:
+                return True
+            else:
+                hset.add(idx)
