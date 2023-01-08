c305432d35c7e342c9d2798a291f43cd1ffc3aa4
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-01-08 07:50:18 +0530

    Create 195.py

---
+++ 195.py
@@ -0,0 +1,6 @@
+    def checkValid(self, matrix: List[List[int]]) -> bool:
+        n = len(matrix)
+        for row, col in zip(matrix, zip(*matrix)):
+            if len(set(row)) != n or len(set(col)) != n:
+                return False
+        return True
